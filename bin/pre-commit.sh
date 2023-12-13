4af08f38184de48344dd7e52dc4d58cd6d9bf7f7
alvi <ahalal82@gmail.com>
2023-12-13 09:56:55 +0300

    LO

--- bin/pre-commit.sh
+++ bin/pre-commit.sh
@@ -1,26 +1,16 @@
 #!/bin/bash
 
-# Сохраняем выводы psalm и php_codesniffer в переменные
-PSALM_OUTPUT=$(vendor/bin/psalm)
-PHPCS_OUTPUT=$(vendor/bin/phpcs)
+# Переходим в директорию с проектом (замените на свой путь)
+cd /var/www/lottery
 
-# Проверяем, есть ли предупреждения в выводе psalm
-if [[ $PSALM_OUTPUT == *"No errors!"* ]]; then
-  echo "Psalm: No errors detected."
-else
-  echo "Psalm: Errors detected. Please fix before committing."
-  echo "$PSALM_OUTPUT"
-  exit 1
+# Запускаем Psalm
+./vendor/bin/psalm
+
+# Проверяем результат выполнения Psalm
+if [ $? -ne 0 ]; then
+    echo "Проверка Psalm не пройдена. Коммит отклонен."
+    exit 1
 fi
 
-# Проверяем, есть ли предупреждения в выводе php_codesniffer
-if [[ $PHPCS_OUTPUT == *"No errors found"* ]]; then
-  echo "PHP_CodeSniffer: No errors detected."
-else
-  echo "PHP_CodeSniffer: Errors detected. Please fix before committing."
-  echo "$PHPCS_OUTPUT"
-  exit 1
-fi
-
-# Если все проверки успешны, разрешаем коммит
+# Если проверка Psalm прошла успешно, продолжаем коммит
 exit 0
