cb3243d879925677b7aea4424446ab76473f6784
alvi <ahalal82@gmail.com>
2023-12-15 23:19:34 +0300

    L0-3

--- src/Core/Worker/Kafka/KafkaQueuedMessage.php
+++ src/Core/Worker/Kafka/KafkaQueuedMessage.php
@@ -8,18 +8,12 @@ use RdKafka\Message;
 
 final readonly class KafkaQueuedMessage
 {
-    /**
-     * @psalm-suppress UndefinedClass
-     */
     public function __construct(
         private Message $message,
         private int $serialNumber
     ) {
     }
 
-    /**
-     * @psalm-suppress UndefinedClass
-     */
     public function getMessage(): Message
     {
         return $this->message;
