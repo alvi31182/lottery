c0121a7962e99ceb1c1aa217396980c64b8f48b0
alvi <ahalal82@gmail.com>
2024-01-04 14:13:44 +0300

    main

--- src/Lottery/Model/LotteryAwardId.php
+++ src/Lottery/Model/LotteryAwardId.php
@@ -6,6 +6,7 @@ namespace App\Lottery\Model;
 
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\ORM\Mapping\Embeddable;
+use InvalidArgumentException;
 use Ramsey\Uuid\Doctrine\UuidGenerator;
 use Ramsey\Uuid\Uuid;
 use Ramsey\Uuid\UuidInterface;
@@ -16,6 +17,9 @@ use Ramsey\Uuid\UuidInterface;
 #[Embeddable]
 class LotteryAwardId
 {
+    /* @var non-empty-string */
+    private const UUID_VERSION = '7';
+
     public function __construct(
         #[ORM\Id]
         #[ORM\Column(type: 'uuid', unique: true, nullable: false)]
@@ -23,6 +27,7 @@ class LotteryAwardId
         #[ORM\CustomIdGenerator(class: UuidGenerator::class)]
         private UuidInterface $id
     ) {
+        $this->validateUuid(uuid: $this->id->toString());
     }
 
     public function getIdString(): string
@@ -40,9 +45,14 @@ class LotteryAwardId
         return Uuid::uuid7();
     }
 
-    public function equals(self $other): bool
+    private function validateUuid(string $uuid): void
     {
-        //ToDo need refactor
-        return $this->id->equals($other->getId());
+        if (!Uuid::isValid($uuid)) {
+            throw new InvalidArgumentException("Invalid UUID format from LotteryAwardId.");
+        }
+
+        if (substr($uuid, 14, 1) !== self::UUID_VERSION) {
+            throw new InvalidArgumentException("Invalid UUID version from LotteryAwardId. Must be version 7.");
+        }
     }
 }
